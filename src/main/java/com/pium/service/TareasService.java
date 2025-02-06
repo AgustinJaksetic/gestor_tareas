662816701c204978f4b8f9401e5add09f012d1ae
Aguspium <agus.jaksetic@gmail.com>
2025-02-06 06:16:43 -0300

    Modularización

--- src/main/java/com/pium/service/TareasService.java
+++ src/main/java/com/pium/service/TareasService.java
@@ -12,16 +12,15 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class TareasService {
-    public static Scanner Scanner = new Scanner(System.in);
+    private final Scanner Scanner = new Scanner(System.in);
+    private static final TareaDao tareaDao = new TareaDao();
     /*
         En TareaService nos encargamos de gestionar la creación de las tareas
         y las validaciones, excepciones, y toda la demás lógica de negocio.
      */
 
-
-    public static void crearTarea(){ //Construimos el objeto Tareas con nuestro patron de diseño Builder y lo insertamos en la base de datos.
-
-        TareaDao.queryTareas(
+    public void crearTarea(){ //Construimos el objeto Tareas con nuestro patron de diseño Builder y lo insertamos en la base de datos.
+        tareaDao.queryTareas(
             new TareasBuilder()
                 .setTitulo("Tarea 1")
                 .setDescripcion("Descripcion 1")
@@ -33,21 +32,19 @@ public class TareasService {
         );
     }
 
-    public static void tituloUpdate(int id, String titulo){ //Actualizamos el título de la tarea indicada por él id.
-        TareaDao.getTarea(id).setTitulo(titulo);
+    public void tituloUpdate(int id, String titulo){ //Actualizamos el título de la tarea indicada por él id.
+        tareaDao.getTarea(id).setTitulo(titulo);
     }
 
-    public static void descripcionUpdate(int id, String descripcion){ //Actualizamos la descripcion de la tarea indicada por él id.
-        TareaDao.getTarea(id).setDescripcion(descripcion);
+    public void descripcionUpdate(int id, String descripcion){ //Actualizamos la descripcion de la tarea indicada por él id.
+        tareaDao.getTarea(id).setDescripcion(descripcion);
     }
 
-    public static List<Actividades> crearActividades(){
+    public List<Actividades> crearActividades(){
         List<Actividades> lista = new ArrayList<>();
         System.out.println("Crear una actividad");
         int a = 1;
         do {
-
-
             lista.add(new Actividades("Actividad "+ a, "Descripcion " + a, a++));
             System.out.println("Crear otra actividad?");
 
@@ -56,20 +53,14 @@ public class TareasService {
         return lista;
     }
 
-    public static HashMap<Integer, Tareas> listaTareas(){
-        return TareaDao.getTareas();
+    public HashMap<Integer, Tareas> listaTareas(){
+        return tareaDao.getTareas();
     }
 
-    public static HashMap<Integer, Tareas> listatareasCompletas() {
-
-        return listaTareas().entrySet().stream()
-
+    public Map<Integer, Tareas> listatareasCompletas() {
+        return tareaDao.getTareas().entrySet().stream()
                 .filter(entry -> entry.getValue().getEstado() == Estado.FINALIZADA)
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (k, v) -> k,
-                        HashMap::new
-                ));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
 }
