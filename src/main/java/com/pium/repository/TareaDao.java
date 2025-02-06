ad41b20e7ee912ec75ce201a08b11695e547101d
Aguspium <agus.jaksetic@gmail.com>
2025-02-06 06:15:37 -0300

    Modularizacion

--- src/main/java/com/pium/repository/TareaDao.java
+++ src/main/java/com/pium/repository/TareaDao.java
@@ -3,22 +3,22 @@ import com.pium.builder.Tareas;
 
 import java.util.HashMap;
 
-public class TareaDao {
+public final class TareaDao {
 
     /*
         TareasDao se encarga de hacer todas las consultas a nuestra
         Base de datos ( en este caso usamos un hashmap como base de datos ).
      */
 
-    public static Tareas getTarea(Integer id) { //Se encarga de hacer una consulta para obtener un objeto Tareas.
+    public Tareas getTarea(Integer id) { //Se encarga de hacer una consulta para obtener un objeto Tareas.
         return TareasDB.db_tareas.get(id);
     }
 
-    public static void queryTareas(Tareas tarea) { //Se encarga de hacer una consulta para insertar un objeto Tareas en la base de datos.
+    public void queryTareas(Tareas tarea) { //Se encarga de hacer una consulta para insertar un objeto Tareas en la base de datos.
         TareasDB.db_tareas.put(tarea.getId(), tarea);
     }
 
-    public static HashMap<Integer, Tareas> getTareas() { //Se encarga de hacer una consulta para obtener todas las tareas de la base de datos.
+    public HashMap<Integer, Tareas> getTareas() { //Se encarga de hacer una consulta para obtener todas las tareas de la base de datos.
         return TareasDB.db_tareas;
     }
 }
