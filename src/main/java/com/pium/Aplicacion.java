12d4e358c95d431abdab8d5bad0735eb9f17311f
Aguspium <agus.jaksetic@gmail.com>
2025-02-06 07:22:35 -0300

    Mejor manejo de scanner

--- src/main/java/com/pium/Aplicacion.java
+++ src/main/java/com/pium/Aplicacion.java
@@ -4,13 +4,16 @@ import com.pium.builder.Tareas;
 import com.pium.service.TareasService;
 
 import java.util.Map;
+import java.util.Scanner;
 
 
 public class Aplicacion {
     public TareasService tareasService = new TareasService();
+    public Scanner scanner = new Scanner(System.in);
+
     public Aplicacion() {
-        tareasService.crearTarea();
-        tareasService.crearTarea();
+        tareasService.crearTarea(scanner);
+        tareasService.crearTarea(scanner);
 
         Map<Integer, Tareas> listas;
         tareasService.tituloUpdate(2, "Titulo Actualizado");
