1a85f6ca9d48dbe9140c21b93e7709748e218333
Aguspium <agus.jaksetic@gmail.com>
2025-03-06 17:57:10 -0300

    refactor

--- src/main/java/com/pium/front/Window.java
+++ src/main/java/com/pium/front/Window.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Window extends JFrame {
 Toolkit toolkit = Toolkit.getDefaultToolkit();
 Dimension screenSize = toolkit.getScreenSize();
-Image icon = toolkit.getImage("src/main/java/com/pium/app/imagenes/pium.png");
+Image icon = toolkit.getImage("src/main/resources/imagenes/icono.png");
     public Window() {
         setBounds(screenSize.width/4, screenSize.height/4, 1200, 800);
         setResizable(false);
